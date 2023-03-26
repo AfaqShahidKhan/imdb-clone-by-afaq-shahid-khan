adc1bda3e16939715567a07c6c5afff1d4617857
Afaq Khan <afaqk3203@gmail.com>
2023-03-26 10:43:35 +0500

    handle the change

--- src/app/search/[searchTerms]/page.jsx
+++ src/app/search/[searchTerms]/page.jsx
@@ -4,7 +4,7 @@ export default async function SearchPage({ params }) {
   const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
   );
-
+  // handle the functionality
   if (!res.ok) {
     throw new Error("Error fetching data");
   }
